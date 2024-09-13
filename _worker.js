4c73fbebf1ee617d4b6c0e4e52b08b1be79e54a4
缘空空 <32029539+NaiHeKK@users.noreply.github.com>
2024-09-14 00:23:36 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -274,7 +274,7 @@ export default {
                         });
 
                     default:
-                        // return new Response('Not found', { status: 404 });
+                        return new Response('Not found', { status: 404 });
                         url.hostname = '';
                         url.protocol = 'https:';
                         request = new Request(url, request);
